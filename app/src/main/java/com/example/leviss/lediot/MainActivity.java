49976457dd5a4c47cb525a1c5bcf33374e75c1c2
Levisoto <levisoto.337@gmail.com>
2018-10-16 19:40:55 -0500

    App working, it can turn ON and turn OFF a led

--- app/src/main/java/com/example/leviss/lediot/MainActivity.java
+++ app/src/main/java/com/example/leviss/lediot/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.leviss.lediot;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
+    private Switch aSwitch;
+    private TextView textView;
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        textView = (TextView) findViewById(R.id.textView);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (aSwitch.isChecked()){
+                    turnOnLed();
+                }else{
+                    turnOffLed();
+                }
+            }
+        });
+
+    }
+
+    private void turnOnLed() {
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dataSnapshot.getRef().child("LED_STATUS").setValue(1);
+                Log.d(TAG, "ON DocumentSnapshot successfully updated!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "ON Falied"+ databaseError.getMessage());
+            }
+        });
+    }
+
+    private void turnOffLed() {
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dataSnapshot.getRef().child("LED_STATUS").setValue(0);
+                Log.d(TAG, "OFF DocumentSnapshot successfully updated!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "OFF Falied" + databaseError.getMessage());
+            }
+        });
     }
 }
